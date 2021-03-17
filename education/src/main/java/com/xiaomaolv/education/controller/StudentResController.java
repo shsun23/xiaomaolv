b4ce3b67e17175dd3491deef6dda193ea0c3c1a5
shsun23 <1532567398@qq.com>
2021-03-17 22:14:59 +0800

    后端代码StudentResController.java修改 post方法获取请求数据的方式

--- education/src/main/java/com/xiaomaolv/education/controller/StudentResController.java
+++ education/src/main/java/com/xiaomaolv/education/controller/StudentResController.java
@@ -26,11 +26,40 @@ public class StudentResController {
 
     }
 
+    /*********** 注意这里的参数解析方法有错误***************
+     * 当前端axios请求是get 方法，并且带有参数，后端使用HttpServletRequest request 接收参数
+     * 并且可以通过String name = request.getParameter("name");获取到请求头中的值
+     * 如：
+     *  getQuestionList(){
+     *     return request({
+     *       url: '/sturesult/question',
+     *       method: 'get',
+     *       params:{
+     *           name: "Bob",
+     *           age: 15
+     *       }
+     *     })
+     *   },
+     *   注意：当前端使用了 params 表示参数是，使用get方法提交，会把参数放到请求头中，method：post不生效
+     *   如果想使用post 方法提交，前端需要使用data：{"name":"zhangsan"}封装数据，后端使用下面方式获取
+     *   public R BriefAnswer(@RequestBody Map params){
+     *       sout(params.get("name"));
+     *   }
+     *   来源：https://www.hangge.com/blog/cache/detail_2485.html
+     *************************************************/
+//    @PostMapping("brief")
+//    public R BriefAnswer(HttpServletRequest request){
+//        System.out.println("进入BRIEF");
+//        String ans = request.getParameter("ans");
+//        System.out.println(ans);
+////        System.out.println(name + ":" + age);
+//        return R.ok().data("ans","1");
+//    }
     @PostMapping("brief")
-    public R BriefAnswer(HttpServletRequest request){
+    public R BriefAnswer(@RequestBody Map params){
         System.out.println("进入BRIEF");
-        String ans = request.getParameter("ans");
-        System.out.println(ans);
+        System.out.println(params.get("ans"));
+//        System.out.println(params.keySet());
 //        System.out.println(name + ":" + age);
         return R.ok().data("ans","1");
     }
